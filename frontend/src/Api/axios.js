dea47e5faf409dc5411c4094eb3545d558aef2c6
kushal-24 <kushalphadnis2015@gmail.com>
2026-01-24 20:31:42 +0530

    change in url in frontend

--- frontend/src/Api/axios.js
+++ frontend/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1", //import.meta.env for vite
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`, //import.meta.env for vite
     withCredentials: true,
     /*
     withCredentials: In browser networking, credentials =
